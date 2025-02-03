72d7740612e617b3f3582ce9870fb770e4db2c76
Diya1201 <sanghvidiya@gmail.com>
2025-02-02 21:44:35 -0500

    commit

--- genai_flask_app/model.py
+++ genai_flask_app/model.py
@@ -1,6 +1,19 @@
+from langchain_ibm import WatsonxLLM
 from langchain_ibm import ChatWatsonx
 from langchain.prompts import PromptTemplate
-from config import PARAMETERS, LLAMA3_MODEL_ID, GRANITE_MODEL_ID, MIXTRAL_MODEL_ID
+from langchain_core.output_parsers import JsonOutputParser
+from pydantic import BaseModel, Field
+from config import PARAMETERS, CREDENTIALS, LLAMA3_MODEL_ID, GRANITE_MODEL_ID, MIXTRAL_MODEL_ID
+
+# Define JSON output structure
+class AIResponse(BaseModel):
+    summary: str = Field(description="Summary of the user's message")
+    sentiment: int = Field(description="Sentiment score from 0 (negative) to 100 (positive)")
+    response: str = Field(description="Suggested response to the user")
+
+# JSON output parser
+json_parser = JsonOutputParser(pydantic_object=AIResponse)
+
 # Function to initialize a model
 def initialize_model(model_id):
     return ChatWatsonx(
@@ -15,27 +28,30 @@ def initialize_model(model_id):
 llama3_llm = initialize_model(LLAMA3_MODEL_ID)
 granite_llm = initialize_model(GRANITE_MODEL_ID)
 mixtral_llm = initialize_model(MIXTRAL_MODEL_ID)
-# Prompt template
+
+# Prompt templates
 llama3_template = PromptTemplate(
     template='''<|begin_of_text|><|start_header_id|>system<|end_header_id|>
-{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>
+{system_prompt}\n{format_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>
 {user_prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
 ''',
-    input_variables=["system_prompt", "user_prompt"]
+    input_variables=["system_prompt", "format_prompt", "user_prompt"]
 )
 
 granite_template = PromptTemplate(
-    template="<|system|>{system_prompt}\n\<|user|>{user_prompt}\n<|assistant|>",
-    input_variables=["system_prompt", "user_prompt"]
+    template="System: {system_prompt}\n{format_prompt}\nHuman: {user_prompt}\nAI:",
+    input_variables=["system_prompt", "format_prompt", "user_prompt"]
 )
 
 mixtral_template = PromptTemplate(
-    template="<s>[INST]{system_prompt}\n{user_prompt}[/INST]",
-    input_variables=["system_prompt", "user_prompt"]
+        template="<s>[INST]{system_prompt}\n{format_prompt}\n{user_prompt}[/INST]",
+    input_variables=["system_prompt", "format_prompt", "user_prompt"]
 )
+
 def get_ai_response(model, template, system_prompt, user_prompt):
-    chain = template | model
-    return chain.invoke({'system_prompt':system_prompt, 'user_prompt':user_prompt})
+    chain = template | model | json_parser
+    return chain.invoke({'system_prompt':system_prompt, 'user_prompt':user_prompt, 'format_prompt':json_parser.get_format_instructions()})
+
 # Model-specific response functions
 def llama3_response(system_prompt, user_prompt):
     return get_ai_response(llama3_llm, llama3_template, system_prompt, user_prompt)
@@ -45,4 +61,3 @@ def granite_response(system_prompt, user_prompt):
 
 def mixtral_response(system_prompt, user_prompt):
     return get_ai_response(mixtral_llm, mixtral_template, system_prompt, user_prompt)
-   
